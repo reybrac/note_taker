da6c66e64e57292f252fc925f3b3d08b793b3588
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-03-12 20:54:32 -0800

    updated folders and file

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // require('./routes/apiRoutes')(app);
-// require('./routes/htmlRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
