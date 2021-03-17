e7683ab7f37b512ed150b8b1ddf1dc2b8214e7b7
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-03-16 22:17:02 -0700

    Fixed the write to file section

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,25 @@
+const fs = require('fs');
 const noteData = require('../db/db.json');
+const path = require('path');
 // const waitListData = require('../data/waitinglistData');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(noteData));
 
-    //app.get('/api/waitlist', (req, res) => res.json(waitListData));
+   
     app.post('/api/notes', (req, res) => {
         
           noteData.push(req.body);
+          fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(noteData));
+          console.log(noteData);
           res.json(true);
     
       });
-    //   app.post('/api/clear', (req, res) => {
-        
-    //     tableData.length = 0;
-    //     waitListData.length = 0;
+    app.post('/api/clear', (req, res) => {
     
-    //     res.json({ ok: true });
-    //   });
+    noteData.length = 0;
+
+
+    res.json({ ok: true });
+    });
 };
