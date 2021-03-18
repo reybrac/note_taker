175744f110c306e25fa90cdd7e3bc1e20fc8afdf
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-03-17 22:48:33 -0700

    Added id to the notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,38 @@
 const fs = require('fs');
-const noteData = require('../db/db.json');
 const path = require('path');
-// const waitListData = require('../data/waitinglistData');
+
+
+
 
 module.exports = (app) => {
+    let noteData = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     app.get('/api/notes', (req, res) => res.json(noteData));
 
    
     app.post('/api/notes', (req, res) => {
+        let noteID;
+        const id = noteID + 1;
         
-          noteData.push(req.body);
-          fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(noteData));
-          console.log(noteData);
-          res.json(true);
+        if (noteData.length) {
+            noteID = Math.max(...(noteData.map(note => note.id)));
+        } else {
+            noteID = 0;
+        }
+        let addedNote = req.body;
+        noteData.push({id, ...addedNote});
+        res.json(noteData.slice(-1));
+        
+        fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(noteData, '\t'));
+        console.log(noteData);
+        //res.json(true);
+        return true;
     
       });
-    app.post('/api/clear', (req, res) => {
-    
-    noteData.length = 0;
+      app.delete('/api/notes/:id', (req, res) => {
+        let currentNotesearch = noteData.find(({ id }) => id === JSON.parse(req.params.id));
 
-
-    res.json({ ok: true });
+       noteData.splice(noteData.indexOf(currentNotesearch), 1);
+        fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(noteData));
+        res.end("Note was deleted");
     });
 };
